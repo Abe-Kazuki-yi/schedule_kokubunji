9c37c9a82ec422906acf07e9f82e1cea7a600c1d
阿部一樹 <ericangyue@gmail.com>
2025-11-03 10:21:08 +0900

    add button to frontend/GetTest

--- frontend/src/pages/GetTest.tsx
+++ frontend/src/pages/GetTest.tsx
@@ -1,7 +1,11 @@
+import { useNavigate } from "react-router-dom";
+
 function GetTest() {
+const navigate = useNavigate();
   return (
     <div>
       <h1>GetTestページです</h1>
+      <button onClick={() => navigate("/")}>homeへ移動</button>
     </div>
   )
 }
