eebad09c3262405e6285bb1bb5d11d34b349d947
阿部一樹 <ericangyue@gmail.com>
2025-11-03 12:19:01 +0900

    change react url to const

---
+++ frontend/src/config/api.ts
@@ -0,0 +1,9 @@
+//バックエンドのベースurl
+export const BASE_URL = "http://localhost:9000";
+
+// APIエンドポイント一覧
+export const ENDPOINTS = {
+    test: "/test",
+    users: "/users",
+    login: "/login",
+  };
